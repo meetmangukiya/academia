258019ef60b0e64ce7732e223701204d993a86b3
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-10-31 23:30:05 +0530

    bst.c: Needs clean up

---
+++ sem-3/DS/bst.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+//#include <conio.h>
+#include <malloc.h>
+
+struct BTNode {
+	struct BTNode* left;
+	struct BTNode* right;
+	struct BTNode* parent;
+	int data;
+};
+
+struct BTNode createBST() {
+	struct BTNode node;
+	node.right = node.left = node.parent = node.data = NULL;
+	return node;
+};
+
+void insert(struct BTNode* node, int data) {
+	if (node->data == NULL) {
+//		struct BTNode* newnode = (struct BTNode*)malloc(sizeof(BTNode));
+		node->data = data;
+		node->left = node->right = NULL;
+	}
+	else {
+		if (data < node->data) {
+			if (node->left == NULL) {
+				struct BTNode* newnode = (struct BTNode*)malloc(sizeof(struct BTNode));
+				newnode->data = data;
+				newnode->left = newnode->right = NULL;
+				newnode->parent = node;
+				node->left = newnode;
+			}
+			else {
+				insert(node->left, data);
+			}
+		}
+		else {
+			if (node->right == NULL) {
+				struct BTNode* newnode = (struct BTNode*)malloc(sizeof(struct BTNode));
+				newnode->data = data;
+				newnode->right = newnode->left = NULL;
+				newnode->parent = node;
+				node->right = newnode;
+			}
+			else {
+				insert(node->right, data);
+			}
+		}
+	}
+}
+
+void traverse_node(struct BTNode* node) {
+    if (node != NULL && node->data == NULL && node->parent == NULL) {
+        printf("Tree is empty");
+        return;
+    }
+
+	if (node != NULL) {
+		traverse_node(node->left);
+		printf("%d ", node->data);
+		traverse_node(node->right);
+	}
+}
+
+struct BTNode* search_tree(struct BTNode* root, int data) {
+    if (root->data == data)
+        return root;
+    else {
+        if (data < root->data) {
+            if (root->left != NULL)
+                search_tree(root->left, data);
+            else
+                return (struct BTNode*)NULL;
+        }
+        else {
+            if (root->right != NULL)
+                search_tree(root->right, data);
+            else
+                return (struct BTNode*)NULL;
+        }
+    }
+}
+
+void delete_node(struct BTNode* node) {
+	if (node->left == NULL && node->right == NULL) {
+        if (node->parent->left == node)
+            node->parent->left = NULL;
+        else
+            node->parent->right = NULL;
+		free(node);
+	}
+	else if (node->left != NULL && node->right != NULL) {
+	    struct BTNode* copy;
+    	copy = node->left;
+		while(copy->right != NULL)
+		    copy = copy->right;
+	    copy->left = node->left;
+    	copy->parent = node->parent;
+		copy->right = node->right;
+		copy->left->parent = copy;
+        copy->right->parent = copy;
+        free(node);
+	}
+	else {
+		if (node->left == NULL) {
+			node->parent->right = node->right;
+			node->right->parent = node->parent;
+			free(node);
+		}
+		else {
+			node->parent->left = node->left;
+			node->left->parent = node->parent;
+			free(node);
+		}
+	}
+}
+
+void main() {
+	struct BTNode root = createBST();
+//	clrscr();
+
+    printf("1: insert, 2: traverse, 3: search, -1: exit");
+
+	while (1) {
+		int choice;
+		printf("Choice: ");
+		scanf("%d", &choice);
+		if (choice == -1) break;
+		switch(choice) {
+			int data;
+			struct BTNode* found;
+			case 1:
+				printf("Data: ");
+				scanf("%d", &data);
+				insert(&root, data);
+				break;
+			case 2:
+				traverse_node(&root);
+				break;
+			case 3:
+			    printf("Data to be searched: ");
+			    scanf("%d", &data);
+			    found = search_tree(&root, data);
+			    if (found == NULL)
+			        printf("Data not found on tree.");
+			    else
+			        printf("Data found!");
+			    break;
+			case 4:
+			    printf("Data to be deleted: ");
+			    scanf("%d", &data);
+			    found = search_tree(&root, data);
+			    if (found != NULL) {
+			        delete_node(found);
+			    }
+			    else {
+			        printf("Data not found on tree.");
+			    }
+			    break;
+
+			case -1:
+				printf("1: insert, 2: traverse, 3: search, -1: exit");
+				break;
+		}
+	}
+
+//	getch();
+}
